d809b2510ee6fd1221368e1fc7df4ef9e352c593
Ali Ghusn <alighusn@outlook.com>
2025-03-06 00:44:34 +0300

    binary search tree

---
+++ Problem_Solving_Strategy/DivideAndConquer/BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿namespace DivideAndConquer;
+public static class BinarySearch
+{
+
+    public static int Iteretaivly(int[] source, int target)
+    {
+        int left = 0;
+        int right = source.Length - 1;
+
+        while (left <= right)
+        {
+            //int mid = (left + right) / 2; // this causing over flow
+            int mid = left + ((right - left) / 2);
+            if (target == source[mid])
+                return mid;
+            else if (target > source[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+
+    public static int Recursively(int[] source, int target, int left, int right)
+    {
+        if (left > right)
+            return -1;
+
+        int mid = left + ((right - left) / 2);
+
+        if (target == source[mid])
+            return mid;
+        else if (target > source[mid])
+            return Recursively(source, target, mid + 1, right);
+        else
+            return Recursively(source, target, left, mid - 1);
+    }
+}
---
+++ Problem_Solving_Strategy/DivideAndConquer/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DivideAndConquer;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = [1, 3, 5, 6, 8, 12, 15, 22, 27, 32, 36, 39, 41, 45, 49, 55, 61, 67, 74, 79, 86];
+
+        var indexIteretaivly = BinarySearch.Iteretaivly(numbers, 67);
+        Console.WriteLine(indexIteretaivly);
+
+        var indexRecursively = BinarySearch.Recursively(numbers, 67, 0, numbers.Length - 1);
+        Console.WriteLine(indexRecursively);
+
+        Console.ReadKey();
+    }
+}
