b29901cbdae81511a6dff12b0275ed4cac6997be
Ali Ghusn <alighusn@outlook.com>
2025-03-06 00:47:00 +0300

    .

--- Problem_Solving_Strategy/DivideAndConquer/obj/Debug/net8.0/DivideAndConquer.AssemblyInfo.cs
+++ Problem_Solving_Strategy/DivideAndConquer/obj/Debug/net8.0/DivideAndConquer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DivideAndConquer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+588a6901398c778267d02bf0d5258a967e4a1ac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DivideAndConquer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DivideAndConquer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
